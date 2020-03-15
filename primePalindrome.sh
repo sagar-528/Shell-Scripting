e22e7427e9bcbacb959d438ae30bf6f01ad18b1d
sagar-528 <gupta.sagar528@gmail.com>
2020-03-15 15:12:22 +0530

    Modified old one

--- primePalindrome.sh
+++ primePalindrome.sh
@@ -4,19 +4,18 @@
 #->Author: Sagar Gupta 
 #->Date: Mar 2020 
 
-echo "enter the number for checking"
-read num
-
+echo -p "enter the number for checking"
+num=$1
 function checkingPrime ()
 {
 	flag=0
-	for (( i=2; i<=$num/2; i++ ))
-	do 
-   	if [[ $num%$i -eq 0 ]]
-		then
-			flag=$(($flag+1))  
-		fi 
-	done
+for (( i=2; i < $num; i++ ))
+do 
+	if [[ $num%$i -eq 0 ]]
+	then
+		((flag++))  
+	fi 
+done
 
 	if [[ $flag -eq 0 ]]
 	then 
@@ -28,51 +27,17 @@ function checkingPrime ()
 
 function checkingPalindrome()
 {
-temp=$num
-while [[ $num -ne 0 ]]
-	do
-		id=$(($num%10))
-		sum=$(( sum*10 ))
-		sum=$((sum+id))
-		num=$((num/10))
-	done
+temp=$1
+sum=0
+while [[ $temp -ne 0 ]]
+do
+	id=$(( $temp % 10 ))
+	sum=$(( $sum * 10 + $id ))
+	temp=$(( $temp / 10 ))
+done
 
-	if [ $sum -eq $temp ]
-	then
-		echo "the number is palindrome"
-	else
-		echo "the number is not palindrome"
-	fi
+checkingPrime $sum
 }
 
-function checkingPrimeAndPalindrome ()
-{
-	temp=$num
-	while [[ $num -ne 0 ]]
-	do
-		id=$(($num%10))
-		sum=$(( sum * 10 ))
-		sum=$(( sum + id ))
-		num=$(( num / 10 ))
-	done
-
-flag=0
-   for (( i=2; i<=$sum/2; i++ ))
-   do 
-      if [[ $sum%$i -eq 0 ]]
-      then
-         flag=$(($flag+1))  
-      fi 
-   done
-
-   if [[ $flag -eq 0 ]]
-   then 
-      echo "it is prime number"
-   else
-      echo "it is not prime number"
-   fi
-
-}
 checkingPrime
 checkingPalindrome
-checkingPrimeAndPalindrome
